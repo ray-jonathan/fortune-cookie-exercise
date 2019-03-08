073f96538f832a821830e40314496650bbd1a678
Jonathan Ray <jonathanray17@gmail.com>
2019-03-08 08:55:32 -0500

    adds arrays of jokes and functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 // Initializing js
+jokes = [
+    "When Chuck Norris goes to donate blood, he declines the syringe, and instead requests a hand gun and a bucket.",
+    "In an average living room there are 1,242 objects Chuck Norris could use to kill you, including the room itself.",
+    "Chuck Norris can hit you so hard that he can actually alter your DNA. Decades from now your descendants will occasionally clutch their heads and yell 'What The Hell was That?'.",
+    "Chuck Norris once shot down a German fighter plane with his finger. By yelling 'Bang!'",
+    "If you spell Chuck Norris in Scrabble, you win. Forever."
+];
+
+fortunes = [
+    "A lie in time saves nine.",
+    "work, n.:\n\tThe blessed respite from screaming kids and\n\tsoap operas for which you actually get paid.",
+    "You can't cheat the phone company.",
+    "You must realize that the computer has it in for you.  The irrefutable\nproof of this is that the computer always does what you tell it to do.",
+    "Beauty:\n\tWhat's in your eye when you have a bee in your hand."
+];
