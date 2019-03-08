8fa0562519372ef81398a6a4b9bc62d04a883690
Jonathan Ray <jonathanray17@gmail.com>
2019-03-08 13:40:05 -0500

    adds background and a looping 'adavance joke button'

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,34 @@
 // Initializing js
-jokes = [
+jokesFortunes = [
     "When Chuck Norris goes to donate blood, he declines the syringe, and instead requests a hand gun and a bucket.",
     "In an average living room there are 1,242 objects Chuck Norris could use to kill you, including the room itself.",
     "Chuck Norris can hit you so hard that he can actually alter your DNA. Decades from now your descendants will occasionally clutch their heads and yell 'What The Hell was That?'.",
     "Chuck Norris once shot down a German fighter plane with his finger. By yelling 'Bang!'",
-    "If you spell Chuck Norris in Scrabble, you win. Forever."
-];
-
-fortunes = [
+    "If you spell Chuck Norris in Scrabble, you win. Forever.",
     "A lie in time saves nine.",
     "work, n.:\n\tThe blessed respite from screaming kids and\n\tsoap operas for which you actually get paid.",
     "You can't cheat the phone company.",
     "You must realize that the computer has it in for you.  The irrefutable\nproof of this is that the computer always does what you tell it to do.",
     "Beauty:\n\tWhat's in your eye when you have a bee in your hand."
 ];
+
+const button = document.querySelector('[data-input');
+const output = document.querySelector('[data-output');
+
+button.addEventListener("click", advanceJoke);
+let count = 0;
+console.log(count);
+
+function advanceJoke(){
+    count +=1;
+    if (count > jokesFortunes.length - 1){
+        count = 0;
+        output.textContent = jokesFortunes[count];
+    }
+    else{
+        output.textContent = jokesFortunes[count];
+    }
+    console.log(count);
+}
+
+output.textContent = jokesFortunes[0];
