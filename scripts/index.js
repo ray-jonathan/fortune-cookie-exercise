80e98218bbc268b8fa89ef868d4143652c1bafaf
Jonathan Ray <jonathanray17@gmail.com>
2019-03-11 15:40:34 -0400

    adds calls directly to API for truly random fortunes

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,27 @@
 // Initializing js
-jokesFortunes = [
-    "When Chuck Norris goes to donate blood, he declines the syringe, and instead requests a hand gun and a bucket.",
-    "In an average living room there are 1,242 objects Chuck Norris could use to kill you, including the room itself.",
-    "Chuck Norris can hit you so hard that he can actually alter your DNA. Decades from now your descendants will occasionally clutch their heads and yell 'What The Hell was That?'.",
-    "Chuck Norris once shot down a German fighter plane with his finger. By yelling 'Bang!'",
-    "If you spell Chuck Norris in Scrabble, you win. Forever.",
-    "A lie in time saves nine.",
-    "work, n.:\n\tThe blessed respite from screaming kids and\n\tsoap operas for which you actually get paid.",
-    "You can't cheat the phone company.",
-    "You must realize that the computer has it in for you.  The irrefutable\nproof of this is that the computer always does what you tell it to do.",
-    "Beauty:\n\tWhat's in your eye when you have a bee in your hand."
-];
 
-const buttonForward = document.querySelector('[data-inputForward');
-const buttonBackward = document.querySelector('[data-inputBackward');
-const buttonRandom = document.querySelector('[data-inputRandom');
-const output = document.querySelector('[data-output');
+
+let jokesFortunes = resultOfFor();
+
+function resultOfFor(){
+    let arrayOfFortunes = [];
+    for (i = 0; i < 10; i++) { 
+        fetch('https://my-little-cors-proxy.herokuapp.com/http://yerkee.com/api/fortune')
+        .then(function (r) { 
+            return r.json(); 
+        }).then(function (fortune) { 
+            arrayOfFortunes.push(fortune.fortune); 
+        });
+    }
+    console.log(arrayOfFortunes);
+    return arrayOfFortunes;
+}
+
+
+const buttonForward = document.querySelector('[data-inputForward]');
+const buttonBackward = document.querySelector('[data-inputBackward]');
+const buttonRandom = document.querySelector('[data-inputRandom]');
+const output = document.querySelector('[data-output]');
 
 buttonForward.addEventListener("click", advanceJoke);
 buttonBackward.addEventListener("click", backJoke);
@@ -56,4 +62,4 @@ function randomJoke(){
 }
 
 // Default joke on page
-output.textContent = jokesFortunes[0];
+output.textContent = "Beauty:\n\tWhat's in your eye when you have a bee in your hand."
